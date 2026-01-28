70aebda19919b0a06d6a70473748e4f511dacc9f
princek987123-bit <princek987123@gmail.com>
2026-01-28 19:02:29 +0530

    initial commit

---
+++ day2.py
@@ -0,0 +1,7 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+print(f"the sum of this two number is : {a + b}")
+print(f"the substraction of this two number is : {a - b}")
+print(f"the multiplicataion of this two number is : {a * b}")
+print(f"the division of this two number is : {a / b}")
