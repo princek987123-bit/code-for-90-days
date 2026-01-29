31eecf7b8899d1cc9fc07cfdd8002706ccd6fa13
princek987123-bit <princek987123@gmail.com>
2026-01-29 09:47:51 +0530

    third code

---
+++ day3.py
@@ -0,0 +1,5 @@
+name = input("Enter your name : ")
+Course = input("Enter your course : ")
+Age = int(input("Enter your age : "))
+
+print(f"Hello! {name}\nYou're pursuing {Course} deegre\nYou're {Age} year old\nAll the best for your future")
